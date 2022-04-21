22db8c49025eda52bf7cd6b629c80c92cf6ee301
Jameson Lopp <jameson@team.casa>
2022-04-21 07:58:57 -0400

    HELLO WORLD

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+
+//
+$(document).ready(function() {
+			$('#fullpage').fullpage({
+				'verticalCentered': false,
+				'scrollingSpeed': 600,
+				'autoScrolling': false,
+				'fitToSection': false,
+				'css3': true,
+				'navigation': true,
+				'navigationPosition': 'right',
+			});
+		});
+
+// wow
+$(function()
+{
+    new WOW().init();
+    $(".rotate").textrotator();
+})
